d93c3552c91f1ce53d93d9166a9e4529885eedbe
Tiago Martins <tiagom87@gmailc.om>
2014-12-03 16:32:40 +0000

    Made test page with task panel

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get 'about' => "pages#about"
+  get 'test' => "pages#test"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
