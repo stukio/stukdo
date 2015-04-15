126f27671f3e222f3bc3e472b189fff483b2fea6
Tiago Martins <tiagom87@gmailc.om>
2015-04-15 22:49:30 +0100

    corrected routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-Rails.application.routes.draw do
-  
+Stukdo::Application.routes.draw do
+  devise_for :users
 
   resources :tasks do
     member do
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     end
   end
 
-  devise_for :users
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
